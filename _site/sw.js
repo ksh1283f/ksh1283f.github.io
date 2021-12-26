c58dc43476de2ba5284433ca9f38d6a06d1cd290
ksh1283f <ksh1283f@naver.com>
2021-12-27 02:23:48 +0900

    211227 change dark mode as a default

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211227.0123';
+const cacheName = 'chirpy-20211227.0216';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
